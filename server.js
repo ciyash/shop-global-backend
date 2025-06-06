7174aaae79797b3a09ab3935ecf62e5f357e5a07
chandra8120 <chandrasekhar8120@gmail.com>
2025-06-06 18:59:37 +0530

    changes

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';    
+import mongoose from 'mongoose';
+import cors from 'cors';  
+import router from './routes/index.js';  
+
+dotenv.config();    
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors({ 
+  origin: "http://localhost:3001",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+app.use(express.json());
+app.use('/api', router);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
