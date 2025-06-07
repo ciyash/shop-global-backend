cc6175a90cc05194098d69281134d93a1d48de56
chandra8120 <chandrasekhar8120@gmail.com>
2025-06-07 10:49:53 +0530

    cors

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors({ 
-  origin: "http://localhost:3001",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
-
+app.use(cors())
 app.use(express.json());
 app.use('/api', router);
 
